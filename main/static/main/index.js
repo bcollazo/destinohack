b8190ced1b5f3f3f518207a5ec41520420e362e9
Bryan Collazo <bcollazo2010@gmail.com>
2018-11-06 22:15:01 -0400

    Search correction

--- main/static/main/index.js
+++ main/static/main/index.js
@@ -1,47 +1,48 @@
-$(document).ready(function() {
-    var defaultBounds = new google.maps.LatLngBounds(
-        new google.maps.LatLng(17.823942, -68.121335),
-        new google.maps.LatLng(18.802253, -65.181211));
+var navigateToGooglePlace = function(place) {
+    var position = place.geometry.location;
+    var lat = position.lat();
+    var lng = position.lng();
+    window.location.href = "/planning?lat=" + lat + "&lon=" + lng;
+};
 
+$(document).ready(function() {
     var input = document.getElementById('searchTextField');
     var options = {
-        bounds: defaultBounds,
+        types: ['(cities)'],
+        componentRestrictions: {country: 'pr'},
+        strictBounds: true,
     };
 
-    autocomplete = new google.maps.places.Autocomplete(input, options);
-    // TODO: Set Fields for billing...
+    var sessionToken = new google.maps.places.AutocompleteSessionToken();
+    var service = new google.maps.places.AutocompleteService();  // For if the user doesn't select from autocomplete
+    var placesService = new google.maps.places.PlacesService(document.createElement('div'));
+    var autocomplete = new google.maps.places.Autocomplete(input, options);
+    autocomplete.setFields(['geometry']);
 
     // Listen for the event fired when the user selects a prediction and retrieve
     // more details for that place.
     autocomplete.addListener('place_changed', function() {
         var place = autocomplete.getPlace();
 
-        console.log(place);
         if (place.geometry) {
-            var position = place.geometry.location;
-            var lat = position.lat();
-            var lng = position.lng();
-            window.location.href = "/planning?lat=" + lat + "&lon=" + lng;
+            navigateToGooglePlace(place);
+        } else {
+            service.getPlacePredictions({
+                input: place.name,
+                sessionToken: sessionToken,
+                ...options,
+            }, function(predictions, status) {
+                if (status != google.maps.places.PlacesServiceStatus.OK) {
+                     alert(status);
+                    return;
+                }
+
+                placesService.getDetails({ placeId: predictions[0].place_id, fields: ['geometry'] }, function(place2, status2) {
+                    if (status == google.maps.places.PlacesServiceStatus.OK) {
+                        navigateToGooglePlace(place2);
+                    }
+                });
+            });
         }
     });
-
-
-// var selections = [];
-// $(".categories div").click(function(event) {
-//     let key = $(this).data('key');
-
-//     var index = selections.indexOf(key);
-//     if (index > -1) {  // Then remove
-//         selections.splice(index, 1);
-//     } else { // else add.
-//         selections.push(key);
-//     }
-//     $(this).toggleClass('selected');
-// });
-
-// $('button').click(function() {
-//     const lat = $("#data").data('lat');
-//     const lon = $("#data").data('lon');
-//     window.location.href = '/planning?categories=' + selections.join(',') + "&lat=" + lat + "&lon=" + lon;
-// });
 });
